b653397ff25fa120b50015d1f269c9ba28258362
ricardoSA54 <ricardosa54@gmail.com>
2025-01-30 08:54:28 +0100

    set up PrimeNG library

--- cv_webpage/src/app/app.config.ts
+++ cv_webpage/src/app/app.config.ts
@@ -4,6 +4,22 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+// PrimeNG
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    // PrimeNG
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
+  ]
 };

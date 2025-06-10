d2c58b274139e2d2f913db873cb4ac496c56e6be
ricardoSA54 <ricardosa54@gmail.com>
2025-06-10 21:09:27 +0200

    push johnDoe websiteConfig

---
+++ cv/profiles/examples/johnDoe/webpage/websiteConfig.js
@@ -0,0 +1,30 @@
+// websiteConfig.js
+export default {
+    "lang": "en",
+    "version": "full",
+    "title": "John Doe's CV",
+    "infoInInHeader": [
+      "email",
+      "phone",
+      "street",
+      "city",
+      "country",
+      "site"
+    ],
+    "sectionsToInclude": [
+      "description",
+      "experience",
+      "education",
+      "popScience",
+      "research",
+      "teaching",
+      "programming",
+      "other",
+      "awards",
+      "certificates",
+      "works",
+      "publications"
+    ],
+    "backgroundTexture": "url(https://media.istockphoto.com/id/1368151405/photo/bamboo-wood-surface-with-texture-and-pattern-light-bamboo-background-for-decorating-furniture.jpg?s=612x612&w=0&k=20&c=kKHgmh8JInES6QlcTIGwHIyef4PSY9ZfJLC5KKUfSYA=)",
+    "pageTexture": "url(./../../../../shared_web/assets/textures/white_paper_1.jpg)",
+}
---
+++ cv/shared_web/scripts/classes/WebsiteConfig.js
@@ -0,0 +1,19 @@
+export class WebsiteConfig {
+    lang;
+    version;
+    title;
+    infoInInHeader;
+    sectionsToInclude;
+    backgroundTexture;
+    pageTexture;
+    constructor({ lang, version, title, infoInInHeader, sectionsToInclude, backgroundTexture, pageTexture, }) {
+        this.lang = lang;
+        this.version = version;
+        this.title = title;
+        this.infoInInHeader = infoInInHeader;
+        this.sectionsToInclude = sectionsToInclude;
+        this.backgroundTexture = backgroundTexture;
+        this.pageTexture = pageTexture;
+    }
+}
+//# sourceMappingURL=WebsiteConfig.js.map

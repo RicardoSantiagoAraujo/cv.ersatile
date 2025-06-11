5de36dffdecc38c6bf016f432c3bc7135ea0a6b8
Ricardo Santiago Araujo <ricardo.santiago-araujo@livingpackets.com>
2025-06-11 11:22:34 +0200

    exclude non-folders from profile list created in the Profiles.py enum

--- cv/scripts/enums/profiles.py
+++ cv/scripts/enums/profiles.py
@@ -9,7 +9,10 @@ excluded_dirs = ["examples"] # directories to exclude from the list of profiles,
 
 profiles = []
 for dir in profile_dirs:
-    profile_names = [f for f in os.listdir(dir) if f not in excluded_dirs]
+    profile_names = [f for f in os.listdir(dir) 
+                     if  os.path.isdir(os.path.join(dir, f))  # check that it is a directory
+                     and f not in excluded_dirs # check that it is not in the excluded directories
+                     ]
     profiles += [(name, dir.replace(base_dir, "") + name) for name in profile_names]
 
 # Dinamically generate enum based on directory list contents

24cb77113c258c08ddaed991898e6c973202ecbd
ricardoSA54 <ricardosa54@gmail.com>
2025-08-07 23:47:43 +0200

    remove wensiteConfig from gitignore

--- cv/profiles/examples/johnDoe/webpage/websiteConfig.js
+++ cv/profiles/examples/johnDoe/webpage/websiteConfig.js
@@ -4,27 +4,28 @@ export default {
     "version": "full",
     "title": "John Doe's CV",
     "infoInInHeader": [
-      "email",
-      "phone",
-      "street",
-      "city",
-      "country",
-      "site"
+        "email",
+        "phone",
+        "street",
+        "city",
+        "country",
+        "site"
     ],
     "sectionsToInclude": [
-      "description",
-      "experience",
-      "education",
-      "popScience",
-      "research",
-      "teaching",
-      "informatics",
-      "other",
-      "awards",
-      "certificates",
-      "works",
-      "publications"
+        "description",
+        "experience",
+        "education",
+        "popScience",
+        "research",
+        "teaching",
+        "informatics",
+        "other",
+        "awards",
+        "certificates",
+        "works",
+        "publications"
     ],
     "backgroundTexture": "rgba(194, 136, 27, 0.35) url(./../../../../shared_web/assets/textures/wood_1.jpg)",
     "pageTexture": "url(./../../../../shared_web/assets/textures/white_paper_1.jpg)",
-}
+};
+//# sourceMappingURL=websiteConfig.js.map

d39ae67cafb073562201b314229314a60908e2c1
Ricardo Santiago Araujo <ricardo.santiago-araujo@livingpackets.com>
2025-09-12 09:18:39 +0200

    remove config file for cvertex from gitignore

---
+++ cv/cv.ertex/config.js
@@ -0,0 +1,15 @@
+export const config = {
+    pdfs: [
+        {
+            profile: "examples/johnDoe",
+            name: "John Doe - Normal version",
+            filename: "cv_johnDoe_FEN"
+        }
+    ],
+    html: [
+        {
+            profile: "examples/johnDoe",
+            name: "John Doe" 
+        }
+    ],
+}
